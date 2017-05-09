a44ea516afdff8e557b3b8ddd837d6a96d6d27c0
Cselo RP <cselo.rp@gmail.com>
2017-05-09 19:20:29 +0200

    MultipleAuth

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,19 @@ Route::resource('categories', 'CategoryController', ['except' => ['create']]);
 Route::resource('tags', 'TagController', ['except' => ['create']]);
 
 Route::get('/', 'pagesController@getIndex');
-
 Route::get('about', 'pagesController@getAbout');
-
 Route::get('contact', 'pagesController@getContact');
 Route::post('contact', 'pagesController@postContact');
 
 Route::resource('post', 'postController');
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
+    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+});
+
 
 Auth::routes();
 
